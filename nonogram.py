e32225b81107ba60b8cbf62ff4ca396422b7e050
csomorbalazs <csomorbp@gmail.com>
2020-12-04 21:48:45 +0100

    Add line solving algorithm

--- nonogram.py
+++ nonogram.py
@@ -11,6 +11,12 @@ def pad(list, target_len):
 
 
 class Nonogram():
+    display_char = {
+        -1: "?",
+        0: "□",
+        1: "■"
+    }
+
     def __init__(self,
                  row_count: int,
                  col_count: int,
@@ -33,11 +39,13 @@ class Nonogram():
         self.col_descriptors = [pad(col, self.col_descriptor_len)
                                 for col in col_descriptors]
 
-        self.cells = [[0 for _ in range(col_count)]
-                      for _ in range(row_count)]
+        self.reset_cells()
 
         if colored_cells:
             self.colored_cells = colored_cells
+
+    def reset_cells(self):
+        self.cells = np.ones((self.row_count, self.col_count), dtype=int) * -1
 
     @property
     def colored_cells(self) -> List[Tuple[int]]:
@@ -75,7 +83,7 @@ class Nonogram():
 
             # body
             for j, col in enumerate(self.col_descriptors):
-                output += "■ " if self.cells[i][j] == 1 else "□ "
+                output += Nonogram.display_char[self.cells[i][j]] + " "
 
             output += "\n"
 
@@ -84,85 +92,120 @@ class Nonogram():
             output = re.sub(r"(?<=[^0-9])0", " ", output)
         print(output)
 
-    def is_valid(self) -> bool:
-        for i in range(len(self.row_descriptors)):
-            row_pieces = []
-            row_open_piece = False
+    def is_solved(self) -> bool:
+        if np.isin(-1, self.cells):
+            return False
 
-            for j in range(len(self.col_descriptors)):
-                if self.cells[i][j] == 1:
-                    if row_open_piece:
-                        row_pieces[-1] += 1
-                    else:
-                        row_pieces.append(1)
-                        row_open_piece = True
-                else:
-                    row_open_piece = False
+        real_row_descriptors = self.__calculate_row_descriptors()
 
-            if pad(row_pieces, self.row_descriptor_len) != self.row_descriptors[i]:
-                return False
+        if real_row_descriptors != self.row_descriptors:
+            return False
 
-        for i in range(len(self.col_descriptors)):
-            col_pieces = []
-            col_open_piece = False
+        real_col_descriptors = self.__calculate_col_descriptors()
 
-            for j in range(len(self.row_descriptors)):
-                if self.cells[j][i] == 1:
-                    if col_open_piece:
-                        col_pieces[-1] += 1
-                    else:
-                        col_pieces.append(1)
-                        col_open_piece = True
-                else:
-                    col_open_piece = False
-
-            if pad(col_pieces, self.col_descriptor_len) != self.col_descriptors[i]:
-                return False
+        if real_col_descriptors != self.col_descriptors:
+            return False
 
         return True
 
     def calculate_descriptors(self):
-        row_descriptors = []
-        col_descriptors = []
-
-        for i in range(self.row_count):
-            row_descriptor = []
-            row_open_piece = False
-
-            for j in range(self.col_count):
-                if self.cells[i][j] == 1:
-                    if row_open_piece:
-                        row_descriptor[-1] += 1
-                    else:
-                        row_descriptor.append(1)
-                        row_open_piece = True
-                else:
-                    row_open_piece = False
-
-            row_descriptors.append(row_descriptor)
-
-        for i in range(self.col_count):
-            col_descriptor = []
-            col_open_piece = False
-
-            for j in range(self.row_count):
-                if self.cells[j][i] == 1:
-                    if col_open_piece:
-                        col_descriptor[-1] += 1
-                    else:
-                        col_descriptor.append(1)
-                        col_open_piece = True
-                else:
-                    col_open_piece = False
-
-            col_descriptors.append(col_descriptor)
-
-        self.row_descriptors = [
-            pad(r, self.row_descriptor_len) for r in row_descriptors]
-        self.col_descriptors = [
-            pad(c, self.col_descriptor_len) for c in col_descriptors]
+        self.row_descriptors = self.__calculate_row_descriptors()
+        self.col_descriptors = self.__calculate_col_descriptors()
 
         return self.row_descriptors, self.col_descriptors
+
+    def __calculate_row_descriptors(self):
+        row_descriptors = []
+
+        for row in self.cells:
+            row_descriptors.append(self.__calculate_descriptor(row))
+
+        return row_descriptors
+
+    def __calculate_col_descriptors(self):
+        col_descriptors = []
+
+        for col in self.cells.T:
+            col_descriptors.append(self.__calculate_descriptor(col))
+
+        return col_descriptors
+
+    def __calculate_descriptor(self, list: List[int]):
+        descriptor = []
+        open_piece = False
+        pad_len = math.ceil(len(list) / 2)
+
+        for elem in list:
+            if elem == 1:
+                if open_piece:
+                    descriptor[-1] += 1
+                else:
+                    descriptor.append(1)
+                    open_piece = True
+            else:
+                open_piece = False
+
+        return pad(descriptor, pad_len)
+
+    def solve_line(self, index: int):
+        if index < self.row_count:
+            row = self.cells[index]
+            row_descriptor = self.row_descriptors[index]
+
+            solved_row = self.__solve_line(row, row_descriptor)
+            reward = np.sum(row == -1) - np.sum(solved_row == -1)
+
+            self.cells[index] = solved_row
+            return reward
+        else:
+            index -= self.row_count
+            col = self.cells[:, index]
+            col_descriptor = self.col_descriptors[index]
+
+            solved_col = self.__solve_line(col, col_descriptor)
+            reward = np.sum(col == -1) - np.sum(solved_col == -1)
+
+            self.cells[:, index] = solved_col
+            return reward
+
+    def __solve_line(self, line, line_descriptor):
+        # More efficient algorithm could be implemented
+        possible_lines = []
+        filled_cell_count = sum(line_descriptor)
+
+        line_indexes = range(len(line))
+
+        for filled_indexes in itertools.combinations(line_indexes, filled_cell_count):
+            possible_solution = [0 for _ in range(len(line))]
+
+            for i in filled_indexes:
+                possible_solution[i] = 1
+
+            if pad(self.__calculate_descriptor(possible_solution),
+                   len(line_descriptor)) == line_descriptor:
+
+                matches_line = True
+                for r, s in zip(line, possible_solution):
+                    if r != -1 and r != s:
+                        matches_line = False
+                        break
+
+                if matches_line:
+                    possible_lines.append(np.array(possible_solution))
+
+        result = [True] * len(possible_lines[0])
+        last_line = possible_lines[0]
+
+        for curr_line in possible_lines:
+            for i, (curr, last) in enumerate(zip(curr_line, last_line)):
+                if curr != last:
+                    result[i] = False
+            last_line = curr_line
+
+        solved_line = [e if result[i] else -1 for i,
+                       e in enumerate(possible_lines[0])]
+
+        return solved_line
 
 
 def random_nonogram(row_count, col_count) -> Nonogram:
@@ -187,9 +230,15 @@ def random_nonogram(row_count, col_count) -> Nonogram:
 
 if __name__ == "__main__":
 
-    nonogram = random_nonogram(row_count=8, col_count=8)
+    nonogram = random_nonogram(5, 5)
+    nonogram.reset_cells()
 
-    nonogram.print()
+    for i in range(1000):
+        nonogram.solve_line(i % 10)
+        nonogram.print()
+        if nonogram.is_solved():
+            print("Solved!")
+            break
 
     print("Row descriptors:", nonogram.row_descriptors)
     print("Column descriptors: ", nonogram.col_descriptors)
